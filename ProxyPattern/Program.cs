bd755dc971a449911688bc7880eea5e57d5673bb
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2025-11-14 16:50:08 +0300

    Proxy Pattern

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+ILogger logger = new BufferedFileLoggerProxy(5);
+logger.Log("Test");
+
+class BufferedFileLoggerProxy : ILogger
+{
+    private readonly int bufferSize;
+    private readonly FileLogger fileLogger;
+    private List<string> buffer;
+
+    public BufferedFileLoggerProxy(int bufferSize)
+    {
+        this.bufferSize = bufferSize;
+        fileLogger = new FileLogger();
+        buffer = new List<string>(capacity: bufferSize);
+    }
+    public void Log(string message)
+    {
+        buffer.Add(message);
+        if (buffer.Count >= bufferSize)
+        {
+            fileLogger.Log(buffer);
+            buffer.Clear();
+        }
+    }
+
+    public void Log(IEnumerable<string> message)
+    {
+        fileLogger.Log(message);
+    }
+}
+
+interface ILogger
+{
+    void Log(string message);
+    void Log(IEnumerable<string> message);
+
+}
+
+class FileLogger : ILogger
+{
+    public void Log(string message)
+    {
+        message = $"[{DateTime.Now:dd.MM.yyyy}- {message}]";
+        File.AppendAllText("message.log", message);
+    }
+    public void Log(IEnumerable<string> message)
+    {
+        File.AppendAllText("message.log", string.Join(Environment.NewLine, message));
+    }
+}
