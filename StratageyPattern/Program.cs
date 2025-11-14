9ef0f0b451a6009efc76c42f8ffb06a0a3c18da5
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2025-11-14 15:32:11 +0300

    Strategy Pattern

---
+++ StratageyPattern/Program.cs
@@ -0,0 +1,49 @@
+﻿
+var character = new Character(new AgrassiveCombatStrategy());
+character.Attack();
+
+character.SetCombatStrategy(new defensiveCombatStrategy());
+character.Attack();
+
+class Character
+{
+    private ICombatStrategy _combatStrategy;
+    public Character()
+    {
+        
+    }
+    public Character(ICombatStrategy combatStrategy)
+    {
+        _combatStrategy = combatStrategy;
+    }
+    public void SetCombatStrategy(ICombatStrategy combatStrategy)
+    {
+        _combatStrategy = combatStrategy;
+    }
+    public void Attack()
+    {
+        _combatStrategy.ExecuteAttack();
+    }
+}
+
+
+interface ICombatStrategy
+{
+    void ExecuteAttack();
+}
+
+class AgrassiveCombatStrategy : ICombatStrategy
+{
+    public void ExecuteAttack()
+    {
+        Console.WriteLine("Performing an aggressive attack!");
+    }
+}
+
+class defensiveCombatStrategy : ICombatStrategy
+{
+    public void ExecuteAttack()
+    {
+        Console.WriteLine("Performing a defensive attack!");
+    }
+}
