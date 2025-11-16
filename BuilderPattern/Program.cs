2a2568563a19ae8c8588a197065581844cd19260
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2025-11-16 10:56:44 +0300

    update

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿
+var eb = new DesignPattern.DesignPattern.BuilderPattern.EndpointBuilder("http://localhost:3000/");
+
+eb.Append("api")
+  .Append("v1")
+  .Append("user")
+  .AppendParam("id", "5")
+  .AppendParam("status", "admin");
+
+var url = eb.Build();
+
+Console.WriteLine("Final URL :" + url);
--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,1 +1,74 @@
-﻿Console.WriteLine("Factory Pattern");
+﻿
+
+
+
+interface IPizza
+{
+    void Prapere();
+    void Bake();
+    void Cut();
+}
+
+class CheesePizza : IPizza
+{
+    public void Bake()
+    {
+        Console.WriteLine("Cheese Pizza Baked");
+    }
+
+    public void Cut()
+    {
+        Console.WriteLine("Cheese Pizza Cut");
+    }
+
+    public void Prapere()
+    {
+        Console.WriteLine("Cheese Pizza Prapered");
+    }
+}
+
+class VegiePizza : IPizza
+{
+    public void Bake()
+    {
+        Console.WriteLine("Cheese Pizza Baked");
+    }
+
+    public void Cut()
+    {
+        Console.WriteLine("Cheese Pizza Cut");
+    }
+
+    public void Prapere()
+    {
+        Console.WriteLine("Cheese Pizza Prapered");
+    }
+}
+
+abstract class PizzaStore
+{
+    protected abstract IPizza CreaterPizze(string type);
+    public IPizza OrderPizza(string type)
+    {
+        IPizza pizza = CreaterPizze(type);
+        pizza.Bake();
+        pizza.Cut();
+        pizza.Prapere();
+
+        return pizza;
+    }
+}
+
+class InstanbulPizzaStore : PizzaStore
+{
+    protected override IPizza CreaterPizze(string type)
+    {
+        return type switch
+        {
+            "cheese" => new CheesePizza(),
+            "veggie" => new VegiePizza(),
+            _ => throw new ArgumentNullException("invalid pizza type", nameof(type)),
+
+        };
+    }
+}
